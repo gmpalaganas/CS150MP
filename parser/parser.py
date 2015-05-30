0bc025ec56eb05ec7218d44e9e5e05e46fbb267b
Genesis Palaganas <itetsky123@gmail.com>
2015-05-30 20:55:08 +0800

    Added almost complete list of tokens

--- parser/parser.py
+++ parser/parser.py
@@ -3,14 +3,31 @@ import ply.yacc as yacc
 
 class Parser:
 
-    literals = "+-*/=!{}[]()"
-
     tokens = (
+            #Identifier
             'IDENT',
-            'INT', 'FLOAT', 'DOUBLE',
+
+            #Constants / Literals
+            'INT', 'FLOAT', 'BOOL'
             'STRING',
-            'AND', 'OR',
-            'BOOL',
+
+            #Operators
+            'PLUS', 'MINUS', 'MULT', 'DIVIDE', 'MOD',
+            'AND', 'OR', 'NOT'
+            'LE', 'GE', 'GT', 'LT', 'EQ', 'NE',
+            'EQUALS', 
+            'MULTEQUALS', 'DIVEQUALS', 'PLUSEQUALS', 'MINUSEQUALS', 'MODEQUALS',
+            'OREQUALS', 'ANDEQUALS',
+            
+            #Increment/Decrement
+            'PLUSPLUS', 'MINUSMINUS',
+            
+            #Delimeters
+            'LPAREN','RPAREN',
+            'LBRACKET', 'RBRACKET',
+            'LBRACE', 'RBRACE'
+            'COMMA', 'SEMI', 'COLON',
+
             )
 
     reserved = {
@@ -31,18 +48,48 @@ class Parser:
 
     tokens = tokens + list(reserved.values())
 
-    precedence = (
-            ('left', '+', '-'),
-            ('left', '*', '/'),
-            ('left', 'AND', 'OR'),
-            ('right', 'UMINUS'),
-            ('right', '!'),
-            )
+    precedence = ()
     
-    t_IDENT = r'[a-zA-z_][a-zA-Z0-9_]*'
+    t_INDENT = r'(numero|tuldok|titik)_[a-zA-z_][a-zA-Z0-9_]*' #Booleans? Strings?
+
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_MULT = r'\*'
+    t_DIVIDE = r'/'
+    t_MOD = r'%'
     t_AND = r'&&'
-    t_OR = r'||'
+    t_OR = r'\|\|'
+    t_NOT = r'!'
+    t_LE = r'<='
+    t_GE = r'>='
+    t_LT = r'<'
+    t_LG = r'>'
+    t_EQ = r'=='
+    t_NE = r'!='
+    t_EQUALS = r'='
+    t_PLUSEQUALS = r'\+='
+    t_MINUSEQUALS = r'-='
+    t_MULTEQUALS = r'\*='
+    t_DIVEQUALS = r'/='
+    t_MODEQUALS = r'%='
+    t_ANDEQUALS = r'&='
+    t_OREQUALS = r'\|='
+
+    t_PLUSPLUS = r'\+\+'
+    t_MINUSMINUS = r'--'
+
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LBRACE = r'\{'
+    t_RBRACE = r'\}'
+    t_COMMA = r','
+    t_SEMI = r';'
+    t_COLON = r':'
+
     t_BOOL = r'true|false'
+    t_ignore = " \t"
 
     def t_STRING(self, t):
         r'"[\w \t]+"'
@@ -59,18 +106,20 @@ class Parser:
             printError("Invalid int value",t)
     
     def t_FLOAT(self,t):
-        r'\d+(.\d+)f'
+        r'\d+(.\d+)'
         try:
             t.value = float(t.value)
         except ValueError:
             printError("Invalid float value",t)
-
-    def t_DOUBLE(self,t):
-        r'\d+(.\d+)'
-        try:
-            t.value = float(t.value)
-            printError("Invalid double value",t)
             
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += t.value.count("\n")
+
+    def t_error(self, t):
+        print "Illegal character '%s'" % t.value[0]
+        t.lexer.skip(1)
+
     def run(self):
         while 1:
             try:
